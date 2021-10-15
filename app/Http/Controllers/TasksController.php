ad9b96f0129c57fbc147d944be28afdcacc50491
Yoko.Yamamoto <skyislimit_2021yo@aol.com>
2021-10-15 16:06:12 +0000

    add column

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Task;
+
+class TasksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //一覧表示
+        $tasks = Task::all();
+        //ビューで表示
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $task = new Task;
+        //タスク作成ビューを表示
+        return view('tasks.create', [
+            'task' => $task,
+            ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //バリデーション
+        $request->validate([
+            'status' => 'required|max:10',   
+        ]);
+        $request->validate([
+            'content' => 'required|max255',   
+        ]);
+        
+        //メッセージを作成
+        $task = new Task;
+        $task->status = $request->status;
+        $task->content = $request->content;
+        $task->save();
+        
+        return redirect('/');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //idの値でタスクを検索して取得
+        $task = Task::findOrFail($id);
+        
+        //タスク詳細ビューで表示
+        return view('tasks.show', [
+                'task' => $task,
+            ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //idの値でタスクを検索して取得
+        $task = Task::findOrFail($id);
+        
+        //タスク編集ビューで表示
+        return view('tasks.edit', [
+                'task' =>$task,
+            ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //バリデーション
+        $request->validate([
+            'status' => 'required|max:10',   
+        ]);
+        $request->validate([
+            'content' => 'required|max:255',   
+        ]);
+        
+        //idの値でタスクを検索して取得
+        $task = Task::findOrFail($id);
+        $task->status = $request->status;
+        $task->content = $request->content;
+        $task->save();
+        
+        return redirect('/');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $task = Task::findOrFail($id);
+        $task->delete();
+        
+        return redirect('/');
+    }
+}
